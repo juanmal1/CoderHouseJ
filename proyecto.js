8bc7c72e86be45826efec603d54eb24e4f1d2d81
juanmal1 <juanmal_100@hotmail.com>
2022-09-27 11:15:16 -0300

    operadores y agregados

--- proyecto.js
+++ proyecto.js
@@ -14,14 +14,19 @@ const producto1 = new producto("Top Cat", 1, 1, "./media/SofiLBdia1win20220138re
 const producto2 = new producto("Top ido", 2, 1, "./media/sofia-Lb-dia-4--win-225639_720x.webp", 15850, 1);
 const producto3 = new producto("Top Cat B", 3, 1, "./media/SofiLBdia2Win20232660rew_720x.webp", 12850, 3);
 
+/*-----------ID traidos del html---------*/
+
 const contenedorCarrito = document.getElementById("carrito-contenedor");
 const botonVaciar = document.getElementById("vaciar-carrito");
 const contadorCarrito = document.getElementById("contadorCarrito");
 const precioTotal = document.getElementById("precioTotal");
 const cantidad = document.getElementById("cantidad");
 const cantidadTotal = document.getElementById("cantidadTotal");
-
+const comprar = document.getElementById("comprar");
 const precioCuotas = document.getElementById("cuotas");
+
+
+/*----storage carrito-----*/
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 console.log(carrito);
@@ -52,12 +57,33 @@ function actualizarCarrito() {
 
     console.log(carrito);
     precioTotal.innerText = carrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0);
+
+    precioCuotas.innerText = carrito.reduce((acc, producto) => acc + producto.precio / 12, 0)
 }
 
 const productos = [];
 
+/*------------- evento dentro del carrito--------*/
+comprar.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Gracias por su compra',
+        text: 'Ya es tuyo!!',
+        ancho:'2em',
+        color:'rgba(207, 26, 108, 0.5)',
+        icon: 'success',
+        confirmButtonText: 'Confirmar'
+    })
+    actualizarCarrito();
+});
 botonVaciar.addEventListener("click", () => {
     carrito.length = 0;
+    
+    Swal.fire({
+        title: 'Seguro que desea eliminar?',
+        text: '',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar'
+    })
     actualizarCarrito();
 });
 
@@ -87,7 +113,21 @@ function mostrarProductos(productos) {
         const boton = document.getElementById(`agregar ${producto.id}`);
 
         boton.addEventListener("click", () => {
+            Toastify({
+                text: "Agregaste el producto al carrito",
+                duration: 2500,
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                background: "linear-gradient(to right, #000000, #771a53be)",
+                },
+                onClick: function(){} // Callback after click
+            }).showToast();
             agregarCarrito(producto.id);
+
         });
     });
 }
@@ -117,10 +157,32 @@ const eliminarCarrito = (prodId) => {
     actualizarCarrito();
 };
 
-function guardar() {
-    let data = localStorage.getItem("carrito");
-    let devolverCarrito = JSON.parse(data);
-}
+// function guardar() {
+//     let data = localStorage.getItem("carrito");
+//     let devolverCarrito = JSON.parse(data);
+// }cS
+
+
+
+
+/*--------Publicidad rapida----------*/
+
+Toastify({
+    text: "No te olvides de registrarte para obetener grandes beneficios",
+    duration: 5000,
+    destination: "#",
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+    background: "linear-gradient(to right, #000000, #771a53be)",
+    },
+    onClick: function(){}
+}).showToast();
+
+
 
 /*----------CARRITO MODEL---------*/
 const contenedorModal = document.getElementsByClassName("modal-contenedor")[0];
